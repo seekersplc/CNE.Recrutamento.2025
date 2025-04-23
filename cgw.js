8bf15d1d3bb27e74ea747385351d84e0d3bc1348
seekersplc <bigmell41@gmail.com>
2025-04-23 23:10:52 +0100

    Create cgw.js

---
+++ cgw.js
@@ -0,0 +1,5 @@
+var password = 'CNE'
+password=prompt('SMS :Bem-vindo ao nosso portal de recrutamento, clique em OK para iniciar a sua candidatura.  ','CNE');
+if (password != 'CNE') {
+location.href='https://seekersplc.github.io/CNE.Recrutamento.2025';
+}
